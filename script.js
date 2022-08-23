942d1d15f8b4da805ebdc480ae2b973c8560763f
eSol-Michelle-Chen <Michelle.Chen@monash.edu>
2022-08-24 09:25:54 +1000

    test

--- script.js
+++ script.js
@@ -36,23 +36,23 @@
 //   })(),
 // });
 
-// AFRAME.registerComponent("rotation-reader", {
-//   /**
-//    * We use IIFE (immediately-invoked function expression) to only allocate one
-//    * vector or euler and not re-create on every tick to save memory.
-//    */
-//   tick: (function () {
-//     var position = new THREE.Vector3();
-//     var quaternion = new THREE.Quaternion();
-//     var direction = new THREE.direction();
+AFRAME.registerComponent("rotation-reader", {
+  /**
+   * We use IIFE (immediately-invoked function expression) to only allocate one
+   * vector or euler and not re-create on every tick to save memory.
+   */
+  tick: (function () {
+    var position = new THREE.Vector3();
+    var quaternion = new THREE.Quaternion();
+    var direction = new THREE.direction();
 
-//     return function () {
-//       var worldPosition = this.el.object3D.getWorldPosition(position);
-//       var worldDirection = this.el.object3D.getWorldDirection();
-//       var quaternion = this.el.object3D.getWorldQuaternion(quaternion);
-//       this.el.object3D.applyQuaternion(quaternion);
-//       this.el.object3D.lookAt(worldDirection);
-//       // this.el.object3D.localToWorld(worldPosition);
-//     };
-//   })(),
-// });
+    return function () {
+      var worldPosition = this.el.object3D.getWorldPosition(position);
+      var worldDirection = this.el.object3D.getWorldDirection();
+      var quaternion = this.el.object3D.getWorldQuaternion(quaternion);
+      this.el.object3D.applyQuaternion(quaternion);
+      this.el.object3D.lookAt(worldDirection);
+      // this.el.object3D.localToWorld(worldPosition);
+    };
+  })(),
+});
